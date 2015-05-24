a573a26d25744d07f8699c71aab139c20e150359
spenserhuang <spenserhuang515@gmail.com>
2015-05-24 02:19:14 -0700

    Completed Seed File and Updated Calendar Migration

--- db/migrate/20150523210723_create_calendars.rb
+++ db/migrate/20150523210723_create_calendars.rb
@@ -2,18 +2,16 @@ class CreateCalendars < ActiveRecord::Migration
   def change
     create_table :calendars do |t|
 
-      t.string :date_value
-      # t.string :name_of_day
-      # t.integer :day_number_of_week
-      t.integer :day_number_of_month
-      # t.integer :day_number_of_year
-      # t.integer :week_number_of_month
-      # t.integer :week_number_of_year
-      # t.string :name_of_month
-      t.integer :month_number_of_year
+      t.string  :date_value
+      t.string  :name_of_day
+      t.integer :day_of_week
+      t.integer :day_of_month
+      t.integer :day_of_year
+      t.integer :week_of_year
+      t.string  :name_of_month
+      t.integer :month_of_year
       t.integer :year_number
-      # t.string :day_type
-      # t.string :holiday
+      t.string  :type_of_day
 
     end
   end
